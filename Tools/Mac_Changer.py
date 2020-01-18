1610489f285fdaf5cfb70540f2c6928a79f7a601
Deathrus <res1m499@hotmail.com>
2020-01-18 11:37:23 -0500

    More improvements

--- Tools/Mac_Changer.py
+++ Tools/Mac_Changer.py
@@ -3,19 +3,19 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-
-(options, arguments) = parser.parse_args()
-
-interface = options.interface
-new_mac = options.new_mac
-
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    return parser.parse_args()
 
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+def change_mac( interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+(options, arguments) = get_arguments()
+change_mac(options.interface, options.new_mac)
