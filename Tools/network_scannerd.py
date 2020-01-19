abf2dea2566802091dffb4808a57734aafa161e3
Deathrus <res1m499@hotmail.com>
2020-01-18 20:36:45 -0500

    Update

---
+++ Tools/network_scannerd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import optparse
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="target", help=" Add IP scan range EG-0.0.0.0/24 to get MACs")
+    (options,arguments) = parser.parse_args()
+    if not options.target:
+        parser.error("[-] Please specify a valid IP, use --help for more info")
+    return options
+
+def scan(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    clients_list = []
+    for element in answered_list:
+        client_dict = {"ip":element[1].psrc, "mac":element[1].hwsrc}
+        clients_list.append(client_dict)
+    return clients_list
+
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n -----------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+options = get_arguments()
+scan_result = scan(options.target)
+print_result(scan_result)
